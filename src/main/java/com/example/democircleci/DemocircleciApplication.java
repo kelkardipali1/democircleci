3e56a2b601021fce3eedfd1baa5cc6e3d2ce9128
kelkardipali1 <kelkardipali1@gmail.com>
2021-01-11 16:29:55 +0530

    first commit

---
+++ src/main/java/com/example/democircleci/DemocircleciApplication.java
@@ -0,0 +1,13 @@
+package com.example.democircleci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocircleciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocircleciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/democircleci/DemocircleciApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.democircleci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemocircleciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
